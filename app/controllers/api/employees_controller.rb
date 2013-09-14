8db2d520233dc98fa0cb88c4be1183cf34b4c2a1
owain lewis <owain@owainlewis.com>
2013-09-14 10:20:33 +0100

    adds api

--- app/controllers/api/employees_controller.rb
+++ app/controllers/api/employees_controller.rb
@@ -1,43 +1,39 @@
-class EmployeesController < ApplicationController
+class Api::EmployeesController < ApplicationController
   
-  before_action :set_employee, only: [:show, :edit, :update, :destroy]
+  before_action :set_employee, only: [:show, :update, :destroy]
   
-  respond_to :html
+  respond_to :json
 
   def index
     @employees = Employee.all
+    respond_with @employees
   end
 
   def show
-  end
-
-  def new
-    @employee = Employee.new
-  end
-
-  def edit
+    respond_with @employee
   end
 
   def create
     @employee = Employee.new(employee_params)
+
     if @employee.save
-      redirect_to @employee, notice: 'Employee was successfully created'
+      render json: @employee, status: :created
     else
-      render action: :new
+      render json: @employee.errors, status: :unprocessable_entity
     end
   end
 
   def update
     if @employee.update(employee_params)
-      redirect_to @employee, notice: 'Employee was successfully updated.'
+      head :no_content, status: :ok
     else
-      render action: :edit
+      render json: @employee.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
     @employee.destroy
-    redirect_to employees_url
+    head :no_content
   end
 
   private
